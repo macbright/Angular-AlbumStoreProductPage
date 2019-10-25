23820281ae0975ef97da782eb46ff445301d2826
bright okike <brightokike@gmail.com>
2019-10-26 00:50:13 +0300

    The fourth module completed

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
+import { Album } from "./album";
+import { Observable } from "rxjs/Observable";
+
 
 @Injectable()
 export class ProductService {
@@ -8,9 +11,7 @@ export class ProductService {
 
   constructor(private _http: Http) {}
 
-  getAlbum(id: number) {
-    return this._http
-      .get(this._albumUrl)
-      .map((response: Response) => response.json());
+  getAlbum(id: number):Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
